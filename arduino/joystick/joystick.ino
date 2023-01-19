ee482953a4507d2d762a1a03641b0602455d532f
James Hodgkinson <james@terminaloutcomes.com>
2023-01-20 09:10:29 +1000

    it works!

--- arduino/joystick/joystick.ino
+++ arduino/joystick/joystick.ino
@@ -17,30 +17,40 @@
 */
 #include <PicoGamepad.h>
 
+
 PicoGamepad gamepad;
+
 
 // 16 bit integer for holding input values
 int val;
 
-const int XPOT = 26;
-const int YPOT = 27;
+// GPIO assignment
+#define XPOT 26
+//#define YPOT 27
+//#define ZPOT 28
 
-const int BUTTON1 = 28;
+const int potPins[] = {
+//  XPOT,
+//  YPOT,
+//  ZPOT,
+ };
+const int numOfPots = 3;
 
-const int potPins[] = { XPOT, YPOT };
-const int numOfPins = 2;
+const int numOfButtonPins = 4;
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(LED_BUILTIN, OUTPUT);
-
-  for (int i = 0; i < numOfPins; i+= 1 ) {
+  int i;
+  for (i = 0; i < numOfPots; i+= 1 ) {
     pinMode(potPins[i], INPUT);
   }
-
+  for (i = 0; i < numOfButtonPins ; i+= 1 ) {
+    pinMode(i, INPUT_PULLUP);
+  }
   // Button on pin
-  pinMode(BUTTON1, INPUT_PULLUP);
+//  pinMode(BUTTON0, INPUT_PULLUP);
 }
 
 // Map analog 0-1023 value from pin to max HID range -32767 - 32767
@@ -50,27 +60,37 @@ int mapPotVal(int val) {
 
 void loop() {
 
-  // Get input value from Pico analog pin
-  val = mapPotVal(analogRead(XPOT));
-  gamepad.SetX(val);
+  #ifdef XPOT
+    val = mapPotVal(analogRead(XPOT));
+    gamepad.SetX(val);
+  #endif
 
-  // Repeat with Y pin
-  val = mapPotVal(analogRead(YPOT));
-  gamepad.SetY(val);
+  #ifdef YPOT
+    val = mapPotVal(analogRead(YPOT));
+    gamepad.SetY(val);
+  #endif
 
-//  gamepad.SetZ(val);
-//  gamepad.SetRx(val);
+  #ifdef ZPOT
+    val = mapPotVal(analogRead(ZPOT));
+    gamepad.SetZ(val);
+  #endif
+
+//  #ifdef RXPOT
+//    val = mapPotVal(analogRead(RXPOT));
+//    gamepad.SetRx(val);
+//  #endif
 //  gamepad.SetRy(val);
 //  gamepad.SetRz(val);
 //  gamepad.SetS0(val);
 //  gamepad.SetS1(val);
 
-  // Set button 0 of 128 by reading button on digital pin 28
-  gamepad.SetButton(0, !digitalRead(28));
+  // Set buttons
+  for (int i=0; i<numOfButtonPins; i++) {
+    gamepad.SetButton(i, !digitalRead(i));
+  }
 
   // Set hat direction, 4 hats available. direction is clockwise 0=N 1=NE 2=E 3=SE 4=S 5=SW 6=W 7=NW 8=CENTER
   // gamepad.SetHat(0, 8);
-
 
   // Send all inputs via HID
   // Nothing is send to your computer until this is called.
